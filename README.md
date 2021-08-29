a8541d4244dd296b1ac49bb198fc2ad42fb2ac8d
prdossantos <paulo.santos@fastconnect.om.br>
2021-08-29 18:28:09 -0300

    feat: commit inicial

--- README.md
+++ README.md
@@ -1,46 +1,39 @@
-# Getting Started with Create React App
+# Gerenciamento de documentos
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Gerenciamento de CPF/CNPJ (CRUD) com a possibilidade filtros, ordenação e marcação de alguns em uma blacklist
 
-## Available Scripts
+## Recursos utilizados
 
-In the project directory, you can run:
+1.  ReactJS
+2.  Boostrap
+3.  Gulp
+4.  Axios
 
-### `yarn start`
+## Como usar localmente
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+1.  Clone este repositório e execute o comando: `npm install`.
+2.  Execute o comando: `npm start`.
+3.  Você precisa da <https://github.com/prdossantos/document-validation-api> rodando
+4.  Por padrão irá iniciar em <http://localhost:3000>.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+## Use o docker :)
 
-### `yarn test`
+Se você tiver o docker e docker-compose, basta seguir os passos.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+1.  Execute o comando: `docker-compose build` 
+2.  Execute o comando: `docker-compose up`,  isso pode demorar um pouco dependendo do seu PC.
+3.  Aguarge a mensagem: `react-app    |   Local:            http://localhost:3000`
+4.  Você precisa da <https://github.com/prdossantos/document-validation-api> rodando
+5.  Por padrão irá iniciar em <http://localhost:3000>.
 
-### `yarn build`
+## Estrutura de pastas
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+O projeto segue a seguinte estrutura.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+```js
+src/
+  - components    # Contém os componentes globais do app
+  - pages         # Contém as páginas de acesso
+  - schemas       # Contém os esquemas da regra de negócio
+  - forms         # Contém os formulários da aplicação
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from "react";
+import Table from "./components/Table/Table";
+import DocumentForm from "./forms/DocumentForm";
+import { Document } from "./schemas/DocumentSchema";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [isLoading, setIsloading] = useState(false);
+    const [items, setItems] = useState<Document[]>([]);
+    const [networkError, setNetworkError] = useState("");
+    const [filterSort, setFilterSort] = useState("asc");
+    const [filterBlacklist, setFilterBlacklist] = useState("");
+
+    const onDelete = (document: Document) => {
+        setIsloading(true);
+        fetch(`${process.env.REACT_APP_API_URL}/document/${document.document}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then((res) => res.json())
+        .then((result) => {
+
+            setItems([...items].filter((item) => item._id !== document._id))
+            setIsloading(false);
+            setNetworkError("");
+        }).catch((e) => {
+            setIsloading(false);
+            setNetworkError(e.message);
+        });
+    }
+
+    const toggleBlacklist = (document: Document) => {
+        setIsloading(true);
+        fetch(`${process.env.REACT_APP_API_URL}/document/${document.document}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({})
+        })
+        .then((res) => res.json())
+        .then((result) => {
+
+            setItems([...items].map((item) => {
+                if (item._id === result.data._id) {
+                    item = result.data;
+                }
+                return item;
+            }))
+            setIsloading(false);
+            setNetworkError("");
+        }).catch((e) => {
+            setIsloading(false);
+            setNetworkError(e.message);
+        });
+    }
+
+    const createDocument = (document: string) => {
+        setIsloading(true);
+        fetch(`${process.env.REACT_APP_API_URL}/document`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ document })
+        })
+        .then((res) => res.json())
+        .then((result) => {
+            const { success, data, error } = result;
+            if (success) {
+                const rows = [...items];
+                rows.push(data);
+                setItems(rows);
+                setNetworkError("");
+            } else {
+                setNetworkError(error);
+            }
+            setIsloading(false);
+        }).catch((e) => {
+            setIsloading(false);
+            setNetworkError(e.message);
+        });
+    }
+
+    const onFilter = ( filter: string, value: string ) => {
+        if( filter === 'sort' ) {
+            setFilterSort(value)
+        }
+        if( filter === 'blacklist' ) {
+            setFilterBlacklist(value)
+        }
+
+    }
+
+    useEffect(() => {
+        setIsloading(true);
+        fetch(`${process.env.REACT_APP_API_URL}/documents?sort=${filterSort}&isBlacklist=${filterBlacklist}`)
+        .then((res) => res.json())
+        .then((result) => {
+            setItems(result.data)
+            setNetworkError("");
+            setIsloading(false);
+        }).catch((e) => {
+            setIsloading(false);
+            setNetworkError(e.message);
+        });
+    }, [filterBlacklist, filterSort])
+
+    return (
+        <div className="container">
+            <div className="d-flex flex-column justify-content-center pt-4">
+                <div className="row col-12 text-center">
+                    <h4>
+                        Gerenciamento de documentos
+                    </h4>
+                </div>
+                {networkError !== "" && <Fragment>
+                    <div className="alert alert-danger" role="alert">
+                        <b>Ops:</b>
+                        <p>{networkError}</p>
+                    </div>
+                </Fragment>}
+                <hr />
+                <div className="row">
+                    <div className="col-12">
+                        <DocumentForm onSubmit={createDocument} />
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 mb-2">
+                        <div className="row">
+                        <label className="col-sm-1 col-form-label">Filtrar: </label>
+                        <div className="col-sm-4">
+                            <select className="form-select form-select-sm " onChange={(ev) => onFilter("blacklist", ev.currentTarget.selectedOptions[0].value)} aria-label="Blacklist">
+                                <option value="">Todos documentos</option>
+                                <option value="true">Está na Blacklist</option>
+                                <option value="false">Não está na blacklist</option>
+                            </select>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-12">
+                        <Table
+                            isLoading={isLoading}
+                            rows={items}
+                            columns={[{name: "Documento", sort: true}, {name: "Blacklist"}, {name:""}]}
+                            onDelete={onDelete}
+                            toggleBlacklist={toggleBlacklist}
+                            onFilter={onFilter}
+                        />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
